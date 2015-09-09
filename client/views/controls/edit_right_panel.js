7e062e0c9c1b61bd88900a8021613f9d38dda085
newfych <newfych@yahoo.com>
2015-09-10 01:02:59 +0300

    pre dark adding

--- client/views/controls/edit_right_panel.js
+++ client/views/controls/edit_right_panel.js
@@ -1,39 +1,64 @@
+divs = ['#create-screen-div', '#delete-screen-div'];
+
 Template.editRightPanel.events({
-    'submit #screen-name-form': function(e) {
+    'click #create-screen-button' : function(e){
+        hideDivs('#create-screen-div');
+    },
+    'click #cancel-create-screen' : function(e){
+        var current_screen = $('#screens-list').val();
+        $("#create-screen-div").hide();
+    },
+    'click #delete-screen-button' : function(e){
+        hideDivs('#delete-screen-div');
+    },
+    'click #cancel-delete-screen' : function(e){
+        var current_screen = $('#screens-list').val();
+        $("#delete-screen-div").show();
+    }
+});
+
+function hideDivs(except_div){
+    for (var i in divs){
+        //var div = $(i);
+        if (divs[i] == except_div) {
+            $(divs[i]).toggle();
+            $('#create-screen-input').focus();
+        } else {
+            $(divs[i]).hide();
+        }
+    }
+};
+
+Template.editRightPanel.events({
+    'submit #create-screen-form': function(e) {
         e.preventDefault();
         var user = Meteor.user().username;
-        var screenName = {
+        var screen_name = {
             username: user,
-            screen_name: $('#screenName').val()
+            screen_name: $('#create-screen-input').val()
         };
-        Screens.insert(screenName);
-        $('#screenNameDiv').hide();
-        $('#screenName').val('');
+        Screens.insert(screen_name);
+        $('#create-screen-div').hide();
+        $('#create-screen-input').val('');
     },
-    'submit #screen-delete-form': function(e) {
+    'submit #delete-screen-form': function(e) {
         e.preventDefault();
-        var current_screen = $('#screensList').val();
-        console.log(current_screen)
-        var currentScreenId = Screens.findOne({screen_name: current_screen})._id;
-        Screens.remove(currentScreenId);
-        $('#screenDeleteDiv').hide();
+        var current_screen = $('#screens-list').val();
+        if (current_screen == 'Main') {
+            alert('Main screen can not be deleted!')
+        } else {
+            var current_screen_id = Screens.findOne({screen_name: current_screen})._id;
+            Screens.remove(current_screen_id);
+            $('#delete-screen-div').hide();
+        }
     }
 });
 
 Template.editRightPanel.events({
-    'click #cancel-delete-screen' : function(e){
-        var current_screen = $('#screensList').val();
-        $("#screenDeleteDiv").style.display='block';
-    }
-});
-
-Template.editRightPanel.events({
-    'change #screensList' : function(e){
-        //if (confirm("Delete this screen?")) {
-            var current_screen = $('#screensList').val();
-            $("#currentScreen").empty();
-            $("#currentScreen").append(current_screen);
-        //}
+    'change #screens-list' : function(e){
+        var current_screen = $('#screens-list').val();
+        $("#currentScreen").empty();
+        $("#currentScreen").append(current_screen);
     }
 });
 
@@ -43,5 +68,8 @@ Template.editRightPanel.helpers({
     },
     controls: function() {
         return Controls.find();
+    },
+    screen_name: function() {
+        return $('#screens-list').val();
     }
 });
